5f6336c28a7fd431897aa007eff6cec3f004f5a6
ORBondar <olegbondar1998@gmail.com>
2019-07-11 14:54:42 +0300

    Added long message text truncation

--- src/components/MessageListItem/MessageListItem.js
+++ src/components/MessageListItem/MessageListItem.js
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "./MessageListItem.module.scss";
 import { Avatar } from "components/Avatar/Avatar";
-import cn from 'classnames';
+import cn from "classnames";
 
 export function MessagesListItem({ label, lastMessage, className }) {
-	const letter = label.split(' ').map(w => w.charAt(0)).join("");
+	const letter = label
+		.split(" ")
+		.map(w => w.charAt(0))
+		.join("");
+
 	return (
 		<li className={cn(styles.container, className)}>
-			<Avatar letter={letter}/>
+			<Avatar letter={letter} />
 			<div className={styles.content}>
 				<div className={styles.username}>{label}</div>
-				<div className={styles.message}>{lastMessage || "You have no messages yet"}</div>
+				<div className={styles.message}>
+					<p>{lastMessage || "You have no messages yet"}</p>
+				</div>
 			</div>
 		</li>
 	);
