e41aa75ac6294fcd8c35fda65a538f92ba444895
ORBondar <olegbondar1998@gmail.com>
2019-07-12 15:12:01 +0300

    Added some styling to search bar input

--- src/pages/Contacts/SearchBar/SearchBar.js
+++ src/pages/Contacts/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { TextInput } from "components";
+import styles from "./SearchBar.module.scss";
 
 export function SearchBar(props) {
 	const [value, setValue] = useState("");
@@ -9,5 +10,13 @@ export function SearchBar(props) {
 		setValue(val);
 	};
 
-	return <TextInput fullWidth placeholder="Search" value={value} onChange={handleChange} />;
+	return (
+		<TextInput
+			fullWidth
+			className={styles.input}
+			placeholder="Search"
+			value={value}
+			onChange={handleChange}
+		/>
+	);
 }
